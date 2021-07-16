4ac33465ac10be5ac4ba02fcca74b0b82a8050c1
bigauke <danielinhares@gmail.com>
2021-07-16 11:52:50 -0300

    atualizando style.css

--- src/index.js
+++ src/index.js
@@ -57,11 +57,19 @@ function Lista() {
     </div>
   );
 }
+function Header() {
+  return <div>Header</div>;
+}
+function Footer() {
+  return <div>Footer</div>;
+}
 
 function App() {
   return (
     <div>
+      <Header />
       <Lista />
+      <Footer />
     </div>
   );
 }
